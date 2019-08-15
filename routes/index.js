b3e0378eff40fb7c66006d4ea74ed97bf9ada9a6
miZye <mi.zyeman@gmail.com>
2019-08-15 19:01:11 +0200

    Initial Commit

--- routes/index.js
+++ routes/index.js
@@ -9,25 +9,26 @@ const URL = 'http://transport.opendata.ch/v1/locations';
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
 /**
  * @param req beinhaltet den parameter in der url
  */
-router.get('/locations', function(req, res, next) {
+router.get('/locations', function(req, res) {
 
     /**
-     * Http-Request
+     * Http-Request zur SBB API
      */
     axios.get(`${URL}?query=${req.query.query}`)
         .then((loc) => {
-            console.log(loc.data);
-
+            /**
+             * Wenn die Anfrage erfolgreich ist, werden nur die Daten zum Front-End weitergeleitet
+             */
             res.send(loc.data);
         })
-        .catch((err) => res.send(err))
+        .catch((err) => res.status(err))
 });
 
 module.exports = router;
